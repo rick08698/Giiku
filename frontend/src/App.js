1b9f75d54288eb272c54d89b41a0743a18c5d2b0
tobiuo <kaisei0000000@gmail.com>
2025-06-19 19:44:14 +0900

    components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Button from "./Button.js";
 function App() {
+  const handleClick = () =>{
+    alert("push button");
+  }
   return (
     <>
       <h1>Hello world</h1>
-      <Button/>
+      <Button type="button" disabled={false} onClick={handleClick}>
+        <span>ボタン</span>
+      </Button>
     </>
   );
 }
---
+++ frontend/src/Button.js
@@ -0,0 +1,14 @@
+import styles from "./components/Button.module.css";
+
+function Button(props) {
+
+    const {type, disabled, children, onClick} = props;
+    
+    return (
+        <button className={styles.button} type={type} disabled={disabled} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
+export default Button;
