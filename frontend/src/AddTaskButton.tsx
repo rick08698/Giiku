fa9e1464e0c64ae7a1bf893f332e2f95225cf4ba
tobiuo <kaisei0000000@gmail.com>
2025-06-20 13:45:02 +0900

    ボタンコンポーネント

---
+++ frontend/src/AddTaskButton.tsx
@@ -0,0 +1,5 @@
+function AddTaskButton(){
+    return<button type="button">a</button>
+}
+
+export default AddTaskButton;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css'
+import AddTaskButton from "./AddTaskButton.tsx"
 
 function App() {
 
   return (
     <>
       <div className='background-container'>
-        
       </div>
+      <AddTaskButton/>
     </>
   )
 }
