c18ef92e1e6e32232fc27892115092b38471d671
tobiuo <kaisei0000000@gmail.com>
2025-06-21 10:55:59 +0900

    alian表示

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,30 @@ import { useState } from 'react';
 
 function App() {
   const [isSettingOpen, setIsSettingOpen] = useState(false);
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  const handleAddTask = (task: string) => {
+    if(task.trim() !== ""){
+      setTasks([...tasks,task]);
+      setIsSettingOpen(false);
+    }
+  }
+
 
   return (
     <div id="example">
-      <img src='./night-sky5.jpg' className='background'/>
-      <img src='./kasei_syusei.png' className='alianimage' />
+      <img src='./night-sky5.jpg' className='background_back'/>
+      <img src='./kasei_syusei.png' className='background_front' />
       <div className='foreground-content'>
         <AddTaskButton
           isSettingOpen={isSettingOpen}
           setIsSettingOpen={setIsSettingOpen}
+          onAddTask={handleAddTask}
         />
       </div>
+      {tasks.length > 0 && <img src='./alian.png' className='alianImage' />}
       
-
+      
     </div>
   )
 }
--- frontend/src/SettingScreen.tsx
+++ frontend/src/SettingScreen.tsx
@@ -1,6 +1,9 @@
 import './temp.css';
+import {useState} from 'react';
+
 type SettingTaskProps = {
     onClose: () => void;
+    onAddTask: (task: string) => void;
 };
 
 const today = new Date();
@@ -11,17 +14,19 @@ const dd = String(today.getDate()).padStart(2, '0');
 const dateString: string = `${yyyy}-${mm}-${dd}`; // → "2025-06-20"
 
 
-function SettingScreen({ onClose }: SettingTaskProps) {
+function SettingScreen({ onClose, onAddTask }: SettingTaskProps) {
+    const [taskInput, setTaskInput] = useState("");
+
     return (
         <div className="modal-overlay">
             <div className="modal-content">
                 <h2>タスク設定</h2>
-                <input type="text" placeholder="タスク内容を入力" />
+                <input type="text" placeholder="タスク内容を入力" value={taskInput} onChange={(e) => setTaskInput(e.target.value)}/>
                 <br />
                 <p>終了日時
                     <input type="date" min={dateString} />
                 </p>
-                <button type='button'>追加</button>
+                <button type='button' onClick={() => onAddTask(taskInput)}>追加</button>
                 <br />
                 <button onClick={onClose}>閉じる</button>
             </div>
